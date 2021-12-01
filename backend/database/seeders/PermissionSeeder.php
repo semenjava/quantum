1092a51bd0c90def357e89d22ecf5230dba0e97d
Semen Yasinskii <semenjava@gmail.com>
2021-12-01 17:48:54 +0200

    QUA-2 RBCA

---
+++ backend/database/seeders/PermissionSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use Illuminate\Database\Seeder;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $manageUser = new Permission();
+        $manageUser->name = 'Read';
+        $manageUser->slug = 'super-admin';
+        $manageUser->save();
+
+        $createTasks = new Permission();
+        $createTasks->name = 'Create Tasks';
+        $createTasks->slug = 'create-tasks';
+        $createTasks->save();
+    }
+}
