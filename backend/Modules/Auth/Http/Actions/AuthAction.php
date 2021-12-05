e41fffe6e1969ed21143dead52765b1b11c40e6e
Semen Yasinskii <semenjava@gmail.com>
2021-12-05 11:55:21 +0200

    migrate

--- backend/Modules/Auth/Http/Actions/AuthAction.php
+++ backend/Modules/Auth/Http/Actions/AuthAction.php
@@ -2,20 +2,10 @@
 
 namespace Modules\Auth\Http\Actions;
 
-use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Http\Request;
 use App\Http\Actions\BaseAction;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 use Modules\Auth\Facades\AuthFacade;
-use Modules\Auth\Http\Requests\LoginRequest;
 use App\Properties\Property;
-use Modules\Auth\Entities\User;
-use Modules\Auth\Entities\AuthToken;
 use Nuwave\Lighthouse\Exceptions\AuthorizationException;
-use Modules\Auth\Repositories\UserRepository;
 
 class AuthAction extends BaseAction
 {
