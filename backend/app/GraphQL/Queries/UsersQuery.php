598c26153f1f28123b38e54cda322a44627942cc
Semen Yasinskii <semenjava@gmail.com>
2021-12-15 19:30:50 +0200

    [QUA-11] Adding the search parameter

--- backend/app/GraphQL/Queries/UsersQuery.php
+++ backend/app/GraphQL/Queries/UsersQuery.php
@@ -2,13 +2,23 @@
 
 namespace App\GraphQL\Queries;
 
+use GraphQL\Type\Definition\ResolveInfo;
+use Modules\Auth\Http\Actions\VerificationAction;
+use Modules\Auth\Http\Requests\VerifyRequest;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
 class UsersQuery
 {
     /**
-     * @param  null  $_
-     * @param  array<string, mixed>  $args
+     * Return a value for the field.
+     *
+     * @param  null  $rootValue Usually contains the result returned from the parent field. In this case, it is always `null`.
+     * @param  mixed[]  $args The arguments that were passed into the field.
+     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context Arbitrary data that is shared between all fields of a single query.
+     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
+     * @return mixed
      */
-    public function __invoke($_, array $args)
+    public function resolve($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
         dd($args);
     }
