f57c7d78e4b9ab4e2520040c346534e4814b7d98
Semen Yasinskii <semenjava@gmail.com>
2021-12-21 17:14:11 +0200

    [QUA-14] fix editUser action

--- backend/Modules/Auth/Http/Requests/EditUserRequest.php
+++ backend/Modules/Auth/Http/Requests/EditUserRequest.php
@@ -3,8 +3,9 @@
 namespace Modules\Auth\Http\Requests;
 
 use App\Http\Requests\BaseFormRequest;
+use App\Models\User;
 
-class EditeRequest extends BaseFormRequest
+class EditUserRequest extends BaseFormRequest
 {
     /**
      * Get the validation rules that apply to the request.
@@ -14,7 +15,12 @@ class EditeRequest extends BaseFormRequest
     public function rules()
     {
         return [
-            'lang' => 'string',
+            'user_id' => 'integer',
+            'name' => 'string',
+            'email' => 'string|email|max:255|regex:/[-0-9a-zA-Z.+]+@[-0-9a-zA-Z.+]+.[a-zA-Z]{2,4}/',
+            'password' => ['min:8','max:25', User::PASSWORD_REGEX],
+            'c_password' => 'required|same:password',
+            'role' => 'required|string',
             'time_zone' => 'string'
         ];
     }
