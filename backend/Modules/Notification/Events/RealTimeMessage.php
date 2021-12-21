368c1841cf6c4aea545b48eeede50a870cd1dfa6
Semen Yasinskii <semenjava@gmail.com>
2021-12-22 01:13:07 +0200

    [QUA-17] websocket run

--- backend/Modules/Notification/Events/RealTimeMessage.php
+++ backend/Modules/Notification/Events/RealTimeMessage.php
@@ -3,12 +3,15 @@
 namespace Modules\Notification\Events;
 
 use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Support\Str;
 
-class RealTimeMessage
+class RealTimeMessage implements ShouldBroadcast
 {
-    use SerializesModels;
+    use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public string $message;
 
@@ -20,5 +23,10 @@ class RealTimeMessage
     public function broadcastOn(): Channel
     {
         return new Channel('events');
+    }
+
+    public function broadcastWith()
+    {
+        return ['message' => Str::random(15)];
     }
 }
--- backend/config/broadcasting.php
+++ backend/config/broadcasting.php
@@ -38,9 +38,9 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => false,
-                'host' => '127.0.0.1',
+                'host' => 'localhost',
                 'port' => 6001,
-                'scheme' => 'http'
+                'scheme' => 'ws'
             ],
         ],
 
