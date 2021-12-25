952d4921778d373eec15e52bffc010a821119e7c
Semen Yasinskii <semenjava@gmail.com>
2021-12-25 19:23:37 +0200

    [QUA-20] create MOdules export to get file path download

---
+++ backend/Modules/Export/Http/Actions/ExportAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\Export\Http\Actions;
+
+use App\Http\Actions\BaseAction;
+use App\Contract\Action;
+use App\Properties\Property;
+
+class ExportAction extends BaseAction implements Action
+{
+    public function run(Property $dto)
+    {
+
+    }
+}
